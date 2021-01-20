14c94b7f4588613f5e7b46a841b3eb972d20cded
michelle.js <51353422+michellee4@users.noreply.github.com>
2021-01-19 23:17:15 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+let firstArray = [];
+let i;
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById("changedWords").style.visibility = "hidden";
+    document.getElementById("ChangedWords").style.display = "none";
+    document.getElementById("start").addEventListener("click",StartProgram, false);
+    document.getElementById("changeWords").addEventListener("click", ChangeWords, false);
+
+});
+
+function StartProgram() {
+    for (let i = 0; i < 3; i++) {
+        firstArray[i]=(prompt("Please enter a word"));
+        let anotherLi = document.createElement("li");
+        anotherLi.appendChild(document.createTextNode(firstArray[i]));
+        document.getElementById("firstUl".appendChild(anotherLi));
+    }
+
+
+        document.getElementById("changeWords").style.visibility = "visible";
+        document.getElementById("start").style.visibility = "hidden";
+
+
+  
+}
+
+
+function ChangeWords() {
+    document.getElementById("GetWords").style.display = "none";
+    document.getElementById("ChangedWords").style.display = "block";
+
+    let secondArray = firstArray.map(function (oneWord) {
+        return oneWord.charAt(oneWord.length - 1) + oneWord.substring(1, oneWord.length - 1);
+
+    });
+
+for (let i = 0; i < 3; i++) {
+    let anotherLi = document.createElement("li");
+    anotherLi.appendChild(document.createTextNode(secondArray[i]));
+    document.getElementById("secondU1").appendChild(anotherLi);
+    }
+}
+
+
+
